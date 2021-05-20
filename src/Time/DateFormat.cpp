11ae51f6eafd72ec1da3e80deff20e5a7f0c43d8
KyuubiRan <KyuubiRan@qq.com>
2021-05-20 21:30:55 +0800

    Reformat code

--- src/Time/DateFormat.cpp
+++ src/Time/DateFormat.cpp
@@ -13,13 +13,13 @@ using std::tm;
 
 string kitsune::GetFormatDate(const string &fmt, const tm &timeinfo) {
     kstring sFmt = fmt;
-    sFmt.regexpReplace("yyyy", 16, "%04d", timeinfo.tm_year + 1900);
-    sFmt.regexpReplace("yy", 16, "%02d", timeinfo.tm_year % 100);
-    sFmt.regexpReplace("MM", 16, "%02d", timeinfo.tm_mon + 1);
-    sFmt.regexpReplace("dd", 16, "%02d", timeinfo.tm_mday);
-    sFmt.regexpReplace("HH", 16, "%02d", timeinfo.tm_hour);
-    sFmt.regexpReplace("hh", 16, "%02d", timeinfo.tm_hour > 12 ? timeinfo.tm_hour - 12 : timeinfo.tm_hour);
-    sFmt.regexpReplace("mm", 16, "%02d", timeinfo.tm_min);
-    sFmt.regexpReplace("ss", 16, "%02d", timeinfo.tm_sec);
+    sFmt.regexpReplace("yyyy", 16, "%04d", timeinfo.tm_year + 1900)
+            .regexpReplace("yy", 16, "%02d", timeinfo.tm_year % 100)
+            .regexpReplace("MM", 16, "%02d", timeinfo.tm_mon + 1)
+            .regexpReplace("dd", 16, "%02d", timeinfo.tm_mday)
+            .regexpReplace("HH", 16, "%02d", timeinfo.tm_hour)
+            .regexpReplace("hh", 16, "%02d", timeinfo.tm_hour > 12 ? timeinfo.tm_hour - 12 : timeinfo.tm_hour)
+            .regexpReplace("mm", 16, "%02d", timeinfo.tm_min)
+            .regexpReplace("ss", 16, "%02d", timeinfo.tm_sec);
     return string(sFmt);
 }
