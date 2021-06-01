27c88fc161fdd0f2c6024b004f91cf8aa289622e
KyuubiRan <KyuubiRan@qq.com>
2021-06-01 19:33:00 +0800

    Add Random

---
+++ src/Random/Random.cpp
@@ -0,0 +1,45 @@
+//
+// Created by KyuubiRan on 2021/5/31.
+//
+
+#include "Random.h"
+#include <random>
+#include <algorithm>
+#include <ctime>
+
+using std::vector;
+using std::mt19937;
+using std::uniform_int_distribution;
+using std::uniform_real_distribution;
+
+mt19937 &Gen() {
+    static mt19937 gen(std::time(nullptr) * 233);
+    return gen;
+}
+
+long kitsune::NextInt32(long from, long to) {
+    uniform_int_distribution<long> distrib(from, to);
+    return distrib(Gen());
+}
+
+long long int kitsune::NextInt64(long long int from, long long int to) {
+    uniform_int_distribution<long long int> distrib(from, to);
+    return distrib(Gen());
+}
+
+float kitsune::NextFloat(float from, float to) {
+    uniform_real_distribution<float> distrib(from, to);
+    return distrib(Gen());
+}
+
+double kitsune::NextDouble(double from, double to) {
+    uniform_real_distribution<double> distrib(from, to);
+    return distrib(Gen());
+}
+
+template<typename T>
+vector<T> &kitsune::VectorRandomSort(vector<T> &v) {
+    if (v.empty()) return v;
+    shuffle(v.begin(), v.end(), Gen());
+    return v;
+}
---
+++ src/Random/Random.h
@@ -0,0 +1,102 @@
+//
+// Created by KyuubiRan on 2021/5/31.
+//
+
+#ifndef KITSUNELIB_RANDOM_H
+#define KITSUNELIB_RANDOM_H
+
+#include <vector>
+
+namespace kitsune {
+    /**
+     * @param from 最小值
+     * @param to 最大值
+     * @return [from,to]之间的随机数
+     */
+    long NextInt32(long from, long to);
+
+    /**
+     * @return [0,1]之间的随机数
+     */
+    inline long NextInt32() { return NextInt32(0, 1); }
+
+    /**
+     * @param to 最大值
+     * @return [0,to]之间的随机数
+     */
+    inline long NextInt32(long to) { return NextInt32(0, to); }
+
+    /**
+     * @param from 最小值
+     * @param to 最大值
+     * @return [from,to]之间的随机数
+     */
+    long long int NextInt64(long long int from, long long int to);
+
+    /**
+     * @param from 最小值
+     * @param to 最大值
+     * @return [0,to]之间的随机数
+     */
+    inline long long int NextInt64(long long int to) { return NextInt64(0, to); }
+
+    /**
+     * @param from 最小值
+     * @param until 最大值
+     * @return [0,1]之间的随机数
+     */
+    inline long long int NextInt64() { return NextInt64(0, 1); }
+
+    /**
+     * @param from 最小值
+     * @param to 最大值
+     * @return [from,to]之间的随机浮点数
+     */
+    float NextFloat(float from, float to);
+
+    /**
+     * @param from 最小值
+     * @param to 最大值
+    * @return [0.0f,to]之间的随机浮点数
+    */
+    inline float NextFloat(float to) { return NextFloat(0.0f, to); }
+
+    /**
+     * @param from 最小值
+     * @param to 最大值
+     * @return [0.0f,1.0f]之间的随机浮点数
+     */
+    inline float NextFloat() { return NextFloat(1.0f); }
+
+    /**
+     * @param from 最小值
+     * @param to 最大值
+     * @return [from,to]之间的随机双精度浮点数
+     */
+    double NextDouble(double from, double to);
+
+    /**
+     * @param from 最小值
+     * @param to 最大值
+     * @return [0.0,to]之间的随机双精度浮点数
+     */
+    inline double NextDouble(double to) { return NextDouble(0.0, to); }
+
+    /**
+     * @param from 最小值
+     * @param to 最大值
+     * @return [0.0,1.0]之间的随机双精度浮点数
+     */
+    inline double NextDouble() { return NextDouble(1.0); }
+
+    /**
+     * 随机排列vector中的元素
+     * @tparam T 类型
+     * @param v vector
+     * @return vector自身
+     */
+    template<typename T>
+    std::vector<T> &VectorRandomSort(std::vector<T> &v);
+}
+
+#endif //KITSUNELIB_RANDOM_H
